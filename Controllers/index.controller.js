ad1b93ebb67328453dd229d2f122d909ce760359
Falcon <laanhvu99@gmail.com>
2020-09-22 03:03:07 +0700

    Update : Interface 2

--- Controllers/index.controller.js
+++ Controllers/index.controller.js
@@ -11,6 +11,7 @@ module.exports = {
         const Found = await User.findOne({ where: { user_name: username } })
         if (!Found) {
             req.flash('error_msg', 'Tên Đăng Nhập Không Tồn Tại')
+            console.log(User.hashPassword('123456789    '));
             res.redirect('/')
         } else if (!User.confirmPassword(password,Found.password)) {
             req.flash('error_msg','Mật Khẩu Không Chính Xác')
